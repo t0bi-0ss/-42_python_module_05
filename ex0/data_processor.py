5237ba8536b3fad5f3a3d17dfe1f83cf274b5468
t0bi-0ss <183855820+t0bi-0ss@users.noreply.github.com>
2026-07-16 14:58:48 +0200

    Continue

--- ex0/data_processor.py
+++ ex0/data_processor.py
@@ -28,3 +28,82 @@ class DataProcessor(ABC):
             return self._internal_data.pop(0)
         else:
             return -1, "Internal data is empty"
+        
+        
+class NumericProcessor(DataProcessor):
+    """DataProcessor subclass for numeric data processing"""
+
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, (int, float, list)):
+            if isinstance(data, list):
+                for item in data:
+                    if not isinstance(item, (int, float)):
+                        return False
+            return True
+        else:
+            return False
+        
+    def ingest(self, data: int | float | list[int | float]) -> None:
+        try:
+            if isinstance(data, list):
+                for item in data:
+                    int(item)
+            else:
+                int(data)
+        except (ValueError, TypeError):
+            print("Got exception: Improper numeric data")
+        else:
+            if isinstance(data, list):
+                self._internal_data.append((self._processing_rank, [str(item) for item in data]))
+            else:
+                self._internal_data.append((self._processing_rank, str(data)))
+            self._processing_rank += 1
+
+
+class TextProcessor(DataProcessor):
+    """DataProcessor subclass for text data processing"""
+
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, (str, list)):
+            if isinstance(data, list):
+                for item in data:
+                    if not isinstance(item, str):
+                        return False
+            return True
+        else:
+            return False
+        
+    def ingest(self, data: str | list[str]) -> None:
+        try:
+            if isinstance(data, list):
+                for item in data:
+                    item + "abc"
+            data + "abc"
+        except TypeError:
+            print("Got exception: Improper text data")
+        else:
+            self._internal_data.append((self._processing_rank, data))
+            self._processing_rank += 1
+
+
+class LogProcessor(DataProcessor):
+    """DataProcessor subclass for log data processing"""
+
+    def validate(self, data: Any) -> bool:
+        if isinstance(data, dict):
+            for key in data.keys():
+                if not isinstance(key, str):
+                    return False
+            for value in data.values():
+                if not isinstance(value, str):
+                    return False
+            return True
+        if isinstance(data, list):
+            for item in data:
+                if not isinstance(item, dict) or not self.validate(item):
+                    return False
+            return True
+        else:
+            return False
+        
+    def ingest()
